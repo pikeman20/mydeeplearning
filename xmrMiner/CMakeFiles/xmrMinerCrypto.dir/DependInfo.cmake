7437b37d4fbb7b2f40e745771b2d136efb3448b8
pikeman20 <pikeman20@users.noreply.github.com>
2017-11-04 09:36:34 +0700

    git

---
+++ xmrMiner/CMakeFiles/xmrMinerCrypto.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/xmrMiner/crypto/aesb.c" "/root/xmrMiner/CMakeFiles/xmrMinerCrypto.dir/crypto/aesb.c.o"
+  "/root/xmrMiner/crypto/c_blake256.c" "/root/xmrMiner/CMakeFiles/xmrMinerCrypto.dir/crypto/c_blake256.c.o"
+  "/root/xmrMiner/crypto/c_groestl.c" "/root/xmrMiner/CMakeFiles/xmrMinerCrypto.dir/crypto/c_groestl.c.o"
+  "/root/xmrMiner/crypto/c_jh.c" "/root/xmrMiner/CMakeFiles/xmrMinerCrypto.dir/crypto/c_jh.c.o"
+  "/root/xmrMiner/crypto/c_keccak.c" "/root/xmrMiner/CMakeFiles/xmrMinerCrypto.dir/crypto/c_keccak.c.o"
+  "/root/xmrMiner/crypto/c_skein.c" "/root/xmrMiner/CMakeFiles/xmrMinerCrypto.dir/crypto/c_skein.c.o"
+  "/root/xmrMiner/crypto/oaes_lib.c" "/root/xmrMiner/CMakeFiles/xmrMinerCrypto.dir/crypto/oaes_lib.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "XMRMINER_LARGEGRID=ON"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "."
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
